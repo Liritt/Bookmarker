5e1a4a0ce5c2c5bfa38245e2ec6dcc7583a18a1c
Lirit <gaetanl2004@gmail.com>
2023-08-25 00:03:07 +0200

    Add get_url with searchbar from name

--- collector.py
+++ collector.py
@@ -1,54 +1,66 @@
+import json
+
 from bs4 import BeautifulSoup
+import requests
+from urllib import parse, request
 
 
-class Mangakakalot:
-    soup = None
+class Manganato:
+    soup: BeautifulSoup = None
+    headers: dict = None
+    base_url: str = None
+    name: str = None
 
-    def __init__(self, html):
-        self.soup = BeautifulSoup(html, 'html.parser')
+    def __init__(self, name):
+        self.base_url = "https://manganato.com"
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"
+        }
+        homepage = requests.get(self.base_url, headers=self.headers)
+        self.soup = BeautifulSoup(homepage.content, 'html.parser')
+        self.name = name
 
-    def get_title(self):
-        return self.soup.find('h1').text
+    def _get_url_with_id(self):
+        return
 
-    def get_manwha_chapters(self):
-        lst_chapters = []
-        chapters = self.soup.select(
-            "div.container-main-left div.panel-story-chapter-list ul.row-content-chapter li.a-h, "
-            "#chapter div div.chapter-list div.row"
-        )
+    def _get_url_with_name_search(self):
+        params = parse.urlencode({"searchword": self.name}).encode("utf-8")
 
-        if len(chapters) > 0:
-            for chapter in chapters:
-                url_element = chapter.find("a") or chapter.find("span a")
-                url = url_element["href"]
+        target_url = request.Request(url="https://manganato.com/getstorysearchjson", data=params, headers=self.headers)
+        x = request.urlopen(target_url)
+        results = json.loads(x.read())
+        name_list = {}
+        for res in results['searchlist']:
+            soup = BeautifulSoup(res['name'], 'html.parser')
+            name_text = soup.get_text()
+            name_list.update({name_text: res['url_story']})
 
-                try:
-                    title_element = chapter.find("a") or chapter.find("span a")
-                    title = title_element["title"]
+        try:
+            url = name_list[self.name.lower()]
+        except KeyError:
+            print(f"{self.name} not found among values {name_list.keys()}")
+            exit(1)
 
-                    name = ""
-                    number = ""
+        return url
 
-                    if "Chapter" in title:
-                        name = title.split("Chapter")[0].strip()
-                        number = title.split("Chapter")[1].strip()
-                    elif "chapter" in title:
-                        name = title.split("chapter")[0].strip()
-                        number = title.split("chapter")[1].strip()
+    def _get_image_url(self, soup: BeautifulSoup) -> str:
+        try:
+            pic = soup.find("span", class_='info-image').find("img")['src']
+        except (IndexError, KeyError):
+            print("Failed to get image")
+            exit(1)
 
-                    if ":" in number:
-                        number = number.split(":")[0].strip()
+        return pic
 
-                    if "chapter" in name:
-                        name = name.split("chapter")[0].strip()
-                    try:
-                        dat_upload = chapter.select_one("span.chapter-time.text-nowrap")
-                        dat_upload = dat_upload["title"]
-                    except Exception as e:
-                        print(dat_upload, name, number)
+    def _get_data(self, chapter_number: int):
+        url = self._get_url_with_name_search()
+        page = requests.get(url, headers=self.headers)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        pic = self._get_image_url(soup)
 
-                    if number and name:
-                        lst_chapters.append({"name": name, "number": number, "url": url, "datUpload": dat_upload})
+        last_updated_chapter = soup.find("ul", class_='row-content-chapter').find("li", class_='a-h')
+        print(last_updated_chapter)
+        return pic
 
-                except Exception as e:
-                    print(e)
+
+
