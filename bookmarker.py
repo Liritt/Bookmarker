5338ee93a6ae61536ba0f9dc676978fc0fbda3b2
Liritt <gaetanl2004@gmail.com>
2023-09-05 23:33:59 +0200

    Basic utilisation of the bot completed, it can now get new chapters when updating a manwha

--- bookmarker.py
+++ bookmarker.py
@@ -1,7 +1,8 @@
+import asyncio
 import re
-from time import sleep
 
 import discord
+from discord.ext import commands
 
 import settings
 from collector import Manganato
@@ -10,10 +11,14 @@ from database import insert_new_comic
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 lst_messages = {}
+lst_bot_messages = {}
+
+bot = commands.Bot(command_prefix="/", intents=intents)
 
 
 @client.event
 async def on_ready():
+    """
     channel = client.get_channel(settings.DISCORD['channel_id'])
     bot_channel = client.get_channel(settings.DISCORD['bot_channel_id'])
     async for message in channel.history(limit=1000):
@@ -27,52 +32,115 @@ async def on_ready():
             continue
 
         title, chapter_number = res
-        comic_data = Manganato(title).get_data(float(chapter_number))
+        comic_data = await Manganato(title).get_data(float(chapter_number))
         if not comic_data:
             # await bot_channel.send(f"Sorry, couldn't find comic '{title}' on manganato :(")
             continue
-        print(comic_data)
+
         insert_new_comic(comic_data, 'manganato')
-        sleep(1)
+        await asyncio.sleep(1)
+    """
 
 
+# region Message events
 @client.event
 async def on_message(message):
-    if message.channel.id == settings.DISCORD['channel_id'] and 'Marqueur' in [role.name for role in
-                                                                               message.author.roles]:
-        lst_messages.update({message.id: message.content})
+    bot_channel = get_channel(settings.DISCORD['bot_channel_id'])
+    if (message.channel.id == settings.DISCORD['channel_id']
+            and 'Marqueur' in [role.name for role in message.author.roles]
+            and not message.author.bot):
+        await process_message(message, bot_channel)
 
 
 @client.event
 async def on_message_edit(before, after):
-    if before.channel.id == settings.DISCORD['channel_id'] and 'Marqueur' in [role.name for role in after.author.roles]:
-        lst_messages.update({before.id: after.content})
+    bot_channel = get_channel(settings.DISCORD['bot_channel_id'])
+    if (before.channel.id == settings.DISCORD['channel_id']
+            and 'Marqueur' in [role.name for role in after.author.roles]
+            and not after.author.bot
+            and before.id in lst_messages):
+        msg = await bot_channel.fetch_message(lst_bot_messages[before.id])
+        await msg.delete()
+        lst_bot_messages.pop(before.id)
+        await process_message(after, bot_channel)
 
 
 @client.event
 async def on_message_delete(message):
-    if message.channel.id == settings.DISCORD['channel_id'] and message.id in lst_messages:
+    bot_channel = get_channel(settings.DISCORD['bot_channel_id'])
+    if (message.channel.id == settings.DISCORD['channel_id']
+            and message.id in lst_messages
+            and not message.author.bot):
         lst_messages.pop(message.id)
+        msg = await bot_channel.fetch_message(lst_bot_messages[message.id])
+        await msg.delete()
+        lst_bot_messages.pop(message.id)
 
 
 def get_details_from_message(message: str) -> tuple[str, str] | None:
     message = message.lower()
-    match = re.search(r'(?: -| –)?\s*(?:chapter|chapitre)\s*(\d+(?:[.,]\d+)?)', message)
 
-    if match:
-        title = message[:match.start()].strip()
-        cleaned_title = re.sub(r'\bvol\.\d+\b', '', title).strip()
-        chapter_number = match.group(1).replace(',', '.')
-        return cleaned_title, chapter_number
+    # Modèle pour correspondre à "chapter/chapitre x.x" ou "title x.x"
+    chapter_match = re.search(r'(?: -| –)?\s*(?:chapter|chapitre)\s*(\d+(?:[.,]\d+)?)', message)
 
-    second_match = re.match(r'^(.*?)\s+(\d+(?:\.\d+)?)$', message)
-    if second_match:
-        title = second_match.group(1).strip()
-        cleaned_title = re.sub(r'\bvol\.\d+\b', '', title).strip()
-        chapter_number = second_match.group(2).replace(',', '.')
-        return cleaned_title, chapter_number
+    if chapter_match:
+        title = message[:chapter_match.start()].strip()
+    else:
+        # Modèle pour correspondre à "title x.x" sans le mot "chapter/chapitre"
+        title_match = re.fullmatch(r'^(.*?)\s+(\d+(?:\.\d+)?)$', message)
+        if not title_match:
+            return None
 
-    return None
+        title = title_match.group(1).strip()
+
+    cleaned_title = re.sub(r'\bvol\.\d+\b', '', title).strip()
+    chapter_number = chapter_match.group(1).replace(',', '.') if chapter_match else title_match.group(2).replace(',',
+                                                                                                                 '.')
+
+    return cleaned_title, chapter_number
+
+
+async def process_message(message, bot_channel):
+    res = get_details_from_message(message.content)
+    if res:
+        lst_messages.update({message.id: message.content})
+        title, chapter_number = res
+        comic_data = await Manganato(title).get_data(float(chapter_number))
+        if not comic_data:
+            bot_message = await bot_channel.send(f"**Sorry, I couldn't find '{title}' on manganato :frowning:**")
+            lst_bot_messages.update({message.id: bot_message.id})
+        else:
+            new_chapters = comic_data['new_chapters']
+            desc = '\n**List of available chapters :**\n\n' if new_chapters else 'Not any new chapters posten yet'
+            nb_new_chapters = len(new_chapters)
+            if nb_new_chapters > 10:
+                new_chapters = new_chapters[nb_new_chapters - 10:nb_new_chapters]
+                desc += "*Only 10 elements are listed at maximum*\n"
+            for new_chapter in new_chapters:
+                desc += f"[{new_chapter['title']}]({new_chapter['url']})\n"
+
+            embed = discord.Embed(
+                colour=discord.Colour.dark_teal(),
+                description=desc,
+                title=comic_data['title'],
+            )
+            embed.set_footer(text="Website : Manganato. Lang : EN")
+            embed.set_author(name="Found on manganato", url=Manganato(title).base_url)
+            embed.set_image(url=comic_data['pic_url'])
+            embed_message = await bot_channel.send(embed=embed)
+            lst_bot_messages.update({message.id: embed_message.id})
+
+            return 'processed'
+
+
+def get_channel(channel_id: int):
+    bot_channel = client.get_channel(channel_id)
+    if not bot_channel:
+        # TODO
+        pass
+    return bot_channel
+
+# endregion
 
 
 client.run(settings.DISCORD['bot_token'])
--- collector.py
+++ collector.py
@@ -1,10 +1,10 @@
+import asyncio
 import json
 import re
-from time import sleep
-from typing import List
+import urllib.error
 
+import aiohttp
 from bs4 import BeautifulSoup
-import requests
 from urllib import parse, request
 
 from database import get_url_from_database
@@ -21,11 +21,9 @@ class Manganato:
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"
         }
-        homepage = requests.get(self.base_url, headers=self.headers)
-        self.soup = BeautifulSoup(homepage.content, 'html.parser')
         self.name = name
 
-    def _get_url_with_name_search(self) -> str | None:
+    async def _get_url_with_name_search(self, session) -> str | None:
         params = parse.urlencode({"searchword": self.name}).encode("utf-8")
 
         target_url = request.Request(url="https://manganato.com/getstorysearchjson", data=params, headers=self.headers)
@@ -35,7 +33,7 @@ class Manganato:
         for res in results['searchlist']:
             soup = BeautifulSoup(res['name'], 'html.parser')
             name_text = soup.get_text()
-            name_list.update({name_text.lower(): res['url_story']})
+            name_list.update({name_text.lower().strip(): res['url_story']})
 
         if not name_list:
             print(f'No comic associated for name "{self.name}"')
@@ -45,33 +43,34 @@ class Manganato:
         try:
             url = name_list[self.name.lower()]
         except KeyError:
-            # In case someone use an alternative name
+            # In case user use an alternative name
             url_list = [res['url_story'] for res in results['searchlist']]
             for url_elt in url_list:
                 if url:
                     break
 
-                page = requests.get(url_elt, headers=self.headers)
-                soup = BeautifulSoup(page.content, 'html.parser')
+                async with session.get(url_elt, headers=self.headers) as response:
+                    page = await response.text()
+
+                soup = BeautifulSoup(page, 'html.parser')
                 alt_names = self._get_alt_names(soup)
                 for alt_name in alt_names:
-                    if self.name.lower() == alt_name.lower():
+                    if self.name.lower() == alt_name.lower().strip():
                         url = url_elt
                         break
-                sleep(1)
+                await asyncio.sleep(1)
 
             if not url:
-                print(name_list, self.name)
                 print(f'Couldn\'t find comic named "{self.name}"')
                 return None
 
         # noinspection PyUnboundLocalVariable
         return url
 
-    def _get_url(self):
+    async def _get_url(self, session):
         url = get_url_from_database(self.name)
         if not url:
-            url = self._get_url_with_name_search()
+            url = await self._get_url_with_name_search(session)
 
         return url
 
@@ -89,46 +88,65 @@ class Manganato:
         match = re.search(pattern, url)
         if match:
             return match.group(1)
-        return None
 
     def _get_alt_names(self, soup: BeautifulSoup):
         table = soup.find('table', class_='variations-tableInfo')
         td_list = table.find('tr').find_all('td')
         if td_list[0].text != 'Alternative :':
-            print('No alternative names found for this comic')
-            return {}
+            return []
 
-        all_alt_names = td_list[1].find('h2').text.split(';')
+        brut_alt_names = td_list[1].find('h2').text
+        if ';' in brut_alt_names:
+            all_alt_names = brut_alt_names.split(';')
+        elif ',' in brut_alt_names:
+            all_alt_names = brut_alt_names.split(',')
+        else:
+            all_alt_names = brut_alt_names
         regex = re.compile(r'^[\u0000-\u00FF]+$')
         cleaned_alt_names = [alt_name.strip() for alt_name in all_alt_names if regex.match(alt_name)]
         return cleaned_alt_names if cleaned_alt_names != set() else {}
 
-    def get_data(self, user_chapter_number: float) -> dict | None:
-        url = self._get_url()
-        if not url:
-            return None
+    async def get_data(self, user_chapter_number: float):
+        async with aiohttp.ClientSession(headers=self.headers) as session:
+            async with session.get(self.base_url, headers=self.headers) as response:
+                homepage = await response.text()
+            self.soup = BeautifulSoup(homepage, 'html.parser')
 
-        page = requests.get(url, headers=self.headers)
-        soup = BeautifulSoup(page.content, 'html.parser')
+            url = await self._get_url(session)
+            if not url:
+                return None
 
-        title = soup.find('h1').text
-        alt_names = self._get_alt_names(soup)
-        pic_url = self._get_picture_url(soup)
+            async with session.get(url, headers=self.headers) as response:
+                page = await response.text()
+            soup = BeautifulSoup(page, 'html.parser')
 
-        all_chapters = soup.find("ul", class_='row-content-chapter').find_all("li", class_='a-h')
-        data = {'title': title, 'alt_names': alt_names, 'pic_url': pic_url, 'url': url}
-        new_chapters = []
-        for chapter in all_chapters:
-            chapter_title = chapter.find('a')['title']
-            chapter_url = chapter.find('a')['href']
-            chapter_number = self._get_chapter_number_from_url(chapter_url)
-            if float(chapter_number) > user_chapter_number:
-                new_chapters.append(
-                    {
-                        'url': chapter_url,
-                        'title': chapter_title,
-                    }
-                )
+            title = soup.find('h1').text
+            alt_names = self._get_alt_names(soup)
+            pic_url = self._get_picture_url(soup)
 
-        data['new_chapters'] = new_chapters
-        return data
+            all_chapters = soup.select("ul.row-content-chapter li.a-h")
+            new_chapters = []
+
+            for chapter in all_chapters:
+                chapter_title = chapter.find('a')['title']
+                chapter_url = chapter.find('a')['href']
+
+                try:
+                    chapter_number = self._get_chapter_number_from_url(chapter_url)
+                except urllib.error.HTTPError:
+                    print('Failed request to manganato, retrying in 30 seconds...')
+                    await asyncio.sleep(30)
+                    return await self.get_data(user_chapter_number)
+
+                if float(chapter_number) > user_chapter_number:
+                    new_chapters.append({'url': chapter_url, 'title': chapter_title})
+
+            data = {
+                'title': title,
+                'alt_names': alt_names,
+                'pic_url': pic_url,
+                'url': url,
+                'new_chapters': new_chapters
+            }
+
+            return data
