a485426cbe1a36ae1c4bcb140ba71c0c33d3bded
Liritt <gaetanl2004@gmail.com>
2023-09-12 23:08:14 +0200

    Start of command

--- bookmarker.py
+++ bookmarker.py
@@ -3,24 +3,24 @@ import re
 
 import discord
 from discord.ext import commands
+from discord.ui import Select, View, RoleSelect
 
 import settings
 from collector import Manganato
 from database import insert_new_comic
 
 intents = discord.Intents.all()
-client = discord.Client(intents=intents)
 lst_messages = {}
 lst_bot_messages = {}
 
 bot = commands.Bot(command_prefix="/", intents=intents)
 
 
-@client.event
+@bot.event
 async def on_ready():
     """
-    channel = client.get_channel(settings.DISCORD['channel_id'])
-    bot_channel = client.get_channel(settings.DISCORD['bot_channel_id'])
+    channel = bot.get_channel(settings.DISCORD['channel_id'])
+    bot_channel = bot.get_channel(settings.DISCORD['bot_channel_id'])
     async for message in channel.history(limit=1000):
         if 'Marqueur' in [role.name for role in message.author.roles]:
             lst_messages.update({message.id: message.content.strip()})
@@ -43,7 +43,7 @@ async def on_ready():
 
 
 # region Message events
-@client.event
+@bot.event
 async def on_message(message):
     bot_channel = get_channel(settings.DISCORD['bot_channel_id'])
     if (message.channel.id == settings.DISCORD['channel_id']
@@ -51,8 +51,10 @@ async def on_message(message):
             and not message.author.bot):
         await process_message(message, bot_channel)
 
+    await bot.process_commands(message)
 
-@client.event
+
+@bot.event
 async def on_message_edit(before, after):
     bot_channel = get_channel(settings.DISCORD['bot_channel_id'])
     if (before.channel.id == settings.DISCORD['channel_id']
@@ -65,7 +67,7 @@ async def on_message_edit(before, after):
         await process_message(after, bot_channel)
 
 
-@client.event
+@bot.event
 async def on_message_delete(message):
     bot_channel = get_channel(settings.DISCORD['bot_channel_id'])
     if (message.channel.id == settings.DISCORD['channel_id']
@@ -134,7 +136,7 @@ async def process_message(message, bot_channel):
 
 
 def get_channel(channel_id: int):
-    bot_channel = client.get_channel(channel_id)
+    bot_channel = bot.get_channel(channel_id)
     if not bot_channel:
         # TODO
         pass
@@ -143,4 +145,46 @@ def get_channel(channel_id: int):
 # endregion
 
 
-client.run(settings.DISCORD['bot_token'])
+@bot.command(name="bookmarker_setup")
+async def bookmarker_setup(ctx):
+    await ctx.send("Hello! I am Bookmarker, the bot able to get the latest chapters of your favorite mangas!")
+
+    guild = bot.get_guild(ctx.guild.id)
+    text_channels = guild.text_channels
+    options_readable_channels = [discord.SelectOption(label=text_channel.name) for text_channel in text_channels if text_channel.permissions_for(guild.me).read_messages]
+
+    async def readable_channels_callback(interaction):
+        if len(interaction.data['values']) > 1:
+            await interaction.response.send_message(f"Understood! From now on, I'll watch the following channels: {', '.join(interaction.data['values'])}")
+        else:
+            await interaction.response.send_message(f"Understood! From now on, I'll watch channel \"{interaction.data['values'][0]}\"")
+
+    select = Select(max_values=len(options_readable_channels), options=options_readable_channels, custom_id="readable_channels")
+    select.callback = readable_channels_callback
+
+    user_channels = View()
+    user_channels.add_item(select)
+    await ctx.send("Select the channel where you want to receive the updates", view=user_channels)
+
+    async def roles_callback(interaction):
+        await interaction.response.send_message(f"Ok, I'll only read messages from users that has the chosen role(s).")
+    roles = RoleSelect(max_values=len(ctx.guild.roles))
+    roles.callback = roles_callback
+    roles_view = View()
+    roles_view.add_item(roles)
+    await ctx.send("Now, select which roles must a user have so that I read his messages", view=roles_view)
+
+    options_writable_channels = [discord.SelectOption(label=text_channel.name) for text_channel in text_channels if text_channel.permissions_for(guild.me).send_messages]
+
+    async def writable_channels_callback(interaction):
+        await interaction.response.send_message(f"Understood! From now on, I'll send updates in channel \"{interaction.data['values'][0]}\"")
+
+    select = Select(options=options_writable_channels, custom_id="writable_channels")
+    select.callback = writable_channels_callback
+
+    bot_channel = View()
+    bot_channel.add_item(select)
+    await ctx.send("Now, select the channel where you want me to write when I get updates on a manga", view=bot_channel)
+
+
+bot.run(settings.DISCORD['bot_token'])
